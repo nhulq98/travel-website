4347f464509ad8f49e31bf52725f66ace5eaf66d
nhu <nhule2031998@gmail.com>
2021-10-07 15:00:12 +0700

    add delete user feature

---
+++ admin/delete_user.php
@@ -0,0 +1,22 @@
+<?php 
+include('../connect/myconnect.php');
+echo "vao hay chưa";
+// kiểm tra id có phải kiểu số ko               // giá trị id phải nhỏ nhất là 1 vì id cho AI tăng tự động trong database
+if (isset($_GET['id']) && filter_var($_GET['id'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+        $id = $_GET['id'];
+        $query = "DELETE FROM tbluser WHERE id={$id}";
+        $result = mysqli_query($conn, $query);
+        if($result)
+        {
+            echo "Xóa thành công";
+        }else
+        {
+            echo "Error deleting record: ". mysqli_error($conn);
+        }
+        header('Location: list_user.php');
+    }
+    else{
+        header('Location: list_user.php');
+    }
+    
+ ?>
